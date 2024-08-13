7f988b54fa964022480a60aa7778e8d186803726
Hp <zainqaimi@gmail.com>
2024-08-13 20:09:45 +0500

    tailwind task-4

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        'blue': '#1fb6ff',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+        'my': '#FAE23A',
+      },
+      fontFamily: {
+        sans: ['Plus Jakarta Sans'],
+        serif: ['Merriweather', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
